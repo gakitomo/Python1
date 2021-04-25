a747c7360b21d168a370429bcd78daa2167a57c6
gakitomo <tmo8002@gmail.com>
2021-04-25 11:06:23 +0900

    Update views.py

--- kanban/views.py
+++ kanban/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("仮トップページ")
-
-def index(request):
   return render(request, "kanban/index.html")
 
 def home(request):

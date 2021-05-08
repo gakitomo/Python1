b6665a2dacd382a6447856d8fd88367c0936e025
gakitomo <tmo8002@gmail.com>
2021-05-08 23:02:05 +0900

    Update views.py

--- kanban/views.py
+++ kanban/views.py
@@ -4,10 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView, CreateView
 from django.views.generic import DetailView, UpdateView
 
 from .forms import UserForm
 from .mixins import OnlyYouMixin
+from .forms import UserForm, ListForm
+from . models import List
 
 def index(request):
   return render(request, "kanban/index.html")
@@ -41,3 +45,13 @@ class UserUpdateView(OnlyYouMixin, UpdateView):
 
   def get_success_url(self):
     return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
+
+class ListCreateView(LoginRequiredMixin, CreateView):
+    model = List
+    template_name = "kanban/lists/create.html"
+    form_class = ListForm
+    success_url = reverse_lazy("kanban:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)

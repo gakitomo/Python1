91741804b39e8fda1ce7cb0e3210ac4de6763f2c
gakitomo <tmo8002@gmail.com>
2021-05-08 23:02:44 +0900

    Update urls.py

--- kanban/urls.py
+++ kanban/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path("home/", views.home, name="home"),
   path("signup/", views.signup, name='signup'),
   path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
-  path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"), 
+  path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
+  path("lists/create/", views.ListCreateView.as_view(), name="lists_create"),
 ]
